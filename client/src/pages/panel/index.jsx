7391f788b2765851d535cacea9ad023dfcbfb741
marinaJane08 <marinajane00@163.com>
2021-02-19 17:54:38 +0800

    nes.css

--- client/src/pages/panel/index.jsx
+++ client/src/pages/panel/index.jsx
@@ -97,7 +97,7 @@ export default (props) => {
             : <View>
               {item.name}
               <Text>{item.category.name}</Text>
-              <Button>{item.status === 1 ? '去组队' : '去统一'}</Button>
+              <Button className="nes-btn">{item.status === 1 ? '去组队' : '去统一'}</Button>
             </View>
           }
         </View>
